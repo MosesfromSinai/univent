6100a912aab32c3d631a7382b55d780ec4630a2a
Moses Avila <mosesavila9@gmail.com>
2024-08-30 15:10:41 -0700

    header update

--- components/ui/shared/Header.tsx
+++ components/ui/shared/Header.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">Header
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="/assets/images/logo.svg" width={128} height={38}
+          alt="Univent logo"
+          />
+        </Link>
+      </div>
+    </header>
   )
 }
 
