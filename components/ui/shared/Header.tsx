b63dde892a60105d78d05a07d1f4a2450386014b
Moses Avila <mosesavila9@gmail.com>
2024-08-30 15:12:11 -0700

    header bug

--- components/ui/shared/Header.tsx
+++ components/ui/shared/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const header = () => {
   return (
-    <header className="w-full border-b">Header
+    <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image src="/assets/images/logo.svg" width={128} height={38}
